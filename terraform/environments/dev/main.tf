114b19f0503d94f2fb7c1a20b853071ee2a2b3b1
Charli <chly-huc@student.s19.be>
2026-04-19 00:35:14 +0200

    removed k8s modules because ArgoCD will handle it

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -7,27 +7,3 @@ module "sqs" {
     source = "../../modules/aws/sqs"
     name = var.sqs_name
 }
-
-module "namespace" {
-    source = "../../modules/k8s/namespace"
-}
-
-module "worker" {
-    source = "../../modules/k8s/worker"
-    namespace = module.namespace.name
-    worker_image = var.worker_image
-    sqs_queue_url = module.sqs.url
-    aws_region = var.aws_region
-    aws_endpoint_url = var.aws_endpoint_url
-    s3_bucket = module.s3.bucket_name
-}
-
-module "api" {
-    source = "../../modules/k8s/api"
-    namespace = module.namespace.name
-    api_image = var.api_image
-    aws_region = var.aws_region
-    aws_endpoint_url = var.aws_endpoint_url
-    s3_bucket = module.s3.bucket_name
-    sqs_queue_url = module.sqs.url
-}
--- terraform/environments/dev/provider.tf
+++ terraform/environments/dev/provider.tf
@@ -1,24 +1,10 @@
 terraform {
   required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 3.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
   }
-}
-
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
-
-provider "helm" {
-    kubernetes = {
-      config_path = "~/.kube/config"
-    }
 }
 
 provider "aws" {
@@ -28,10 +14,10 @@ provider "aws" {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_requesting_account_id  = true
-    s3_use_path_style = true
+    s3_use_path_style           = true
 
     endpoints {
       s3  = "http://localhost:4566"
       sqs = "http://localhost:4566"
     }
-  }
+}
